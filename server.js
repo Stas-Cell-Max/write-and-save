8919413abcf38346e590bc35487d917bb1f51513
Stanislav Morozan <stanislav.morozan@gmail.com>
2023-12-15 19:20:42 -0500

    Set up the routes to rerve the HTML files

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { v4: uuidv4 } = require('uuid'); // Import the uuid package to generate u
 
 
 const app = express();                 // Create an Express application instance
-const PORT = process.env.PORT || 3000; //the port on which the server will listen.
+const PORT = process.env.PORT || 3000; //The port on which the server will listen.
 
 
 app.use(express.json());               // Middleware for parsing JSON and urlencoded form data
@@ -19,3 +19,12 @@ app.use(express.static('public'));
 app.listen(PORT, () => {               // Start the server and have it listen on the specified PORT. 
   console.log(`Server is listening on port ${PORT}`);
 });
+
+app.get('/notes', (req, res) => {      // GET Route for note page
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+  
+app.get('*', (req, res) => {         // GET Route for home page
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
